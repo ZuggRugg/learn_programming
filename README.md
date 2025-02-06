cfe1b896bdc1f938c42395f28be72531f3d2e48b
ZuggRugg <shayne.tman@gmail.com>
2025-02-06 12:55:57 -0600

    chapter 9 work finished

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 # Practice files
 
-These are my practice files to help introduce myself to more advanced topics in C++ like
-data structures and namespaces, etc. also to help me with memory management concepts like pointers and malloc()
+These are my practice files to help introduce myself to languages like Python, C++, C
 
 
 # LearnCpp
 chap 4 (finished)
-
 chap 5 (finished)
-
 chap 6 (finished)
-
 chap 7 (finished)
-
 chap 8 (finished)
+chap 9 (finished)
 
 
 # Python
-basic syntax
+Python3 Documentation Chapters 3-4 Done
--- learn_Cpp/chap9.cpp
+++ learn_Cpp/chap9.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cassert>
+
 
 //sometimes users can use programs in ways we never thought possible
 // Learn Cpp Chapter 9
@@ -8,7 +10,7 @@
 //write programs in short well defined units and methods that make it easy for testing
 
 std::string getInput();
-
+double getDouble();
 bool isLowerVowel(char c);
 
 int main() {
@@ -19,6 +21,7 @@ int main() {
   std::cout << f << "\n";
   std::cout << f << "\n";
   std::cout << f << "\n";
+  // std::exit(1); exits the program with an abnormal code
   
   // asserts are a better way of testing programs as it will abort the compilation if there is an error
   //there are entire frameworks called unit testing frameworks built to test code
@@ -30,18 +33,29 @@ int main() {
   // 4) Integer division giving wrong value
   // 5) accidental Null statements
   // 6) String input (Whitespace, Leading Characters, etc.)
-  
+
+
+  //final thing
+  // asserts() evaluate to true whenever the statement is true, in that case the assert does nothing
+  // in the other case then it throws an error and aborts the program
+
+  //for error logging
+  std::cerr << "error" << "\n";
+
 
   //Input Validation (make sure that the Input given by the User is correct)
 
   // std::string Myname = getInput();
+  // std::string ws;
+  // std::cout << "Input a name right now: ";
+  // getline(std::cin, ws);
+  // std::cout << ws << "\n";
 
-  // std::cout << Myname;
-
+  assert(isLowerVowel('G') && "the program has exited succesfully with a assert error checking fuction, thanks for chapter 9");
   return 0;
 }
 
-//function to test that something is the correct input or output
+// function to test that something is the correct input or output
 bool isLowerVowel(char c)
 {
     switch (c)
@@ -65,3 +79,27 @@ std::string getInput() {
 
   return Myname;
 }
+
+
+double getDouble()
+{
+    while (true) // Loop until user enters a valid input
+    {
+        std::cout << "Enter a decimal number: ";
+        double x{};
+        std::cin >> x;
+
+        if (!std::cin) // If the previous extraction failed
+        {
+            // Let's handle the failure
+            std::cin.clear(); // Put us back in 'normal' operation mode
+            continue;
+        }
+
+        // Our extraction succeeded
+        return x;     // Return the value we extracted
+    }
+}
+
+
+
