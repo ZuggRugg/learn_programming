0e5d2a5a7e74a379de0822f08c3b54ec96cd1d7d
ZuggRugg <shayne.tman@gmail.com>
2025-03-05 20:22:34 -0600

    chapter 13 work

--- learn_Cpp/Chap13.cpp
+++ learn_Cpp/Chap13.cpp
@@ -1,10 +1,65 @@
 #include <iostream>
-
+#include <cstdint>
+#include <string>
 // Chapter 13 of the LearnCPP tutorial
+// Enumerations and Pointer shit
+// Enumerations are good to use when you want to associate words with integer values quickly
 
-int main() {
-  std::cout << "hello ";
-  return 0;
+
+enum poopfart {
+  poop  = 1,     // is == 1  
+  fart  = 2,	    // is == 2 
+  shit = 69,	    // is == 69 
+  shart = 4,    // is == 4 
+  vomit = 5,    // is == 5 
+}; 
+
+enum Color : uint16_t
+{
+  red,
+  blue,
+  green,
+  yellow,
+};
+
+std::string getColor(poopfart object);
+
+int main(void) {
+  std::cout << "hello \n";
+
+  poopfart bitch = shart;
+  Color duh = yellow;
+
+  // overload an << operator to print an enum
+  std::string newstr = getColor(bitch);
+
+  std::cout << newstr << "\n";
+  std::cout << yellow;
+
+ //------------------------------
+ //TODO: Chapter 13.7 (Structs) |
+ //------------------------------
+
+
 }
+
+
+
+
+std::string getColor(poopfart object) {
+  switch(object) {
+  case poop:   return  "poop";       break;
+  case fart:   return  "fart";       break;
+  case shart:  return  "shart";      break;
+  case shit:   return  "shit";       break;
+  default: return "couldn't find relevant statement in enum"; break;
+  }
+}
+
+
+
+
+
+
 
 
